4cc53ac6873d66ef96aa55e8e2805e1d83d08290
Yusyo8554 <98458010+Yusyo8554@users.noreply.github.com>
2026-04-29 09:07:22 +0900

    はじめてのコミット

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("btn") // IDが "btn" の要素をHTMLから取得し、button という変数に入れる
+const message = document.getElementById("message") // IDが "message" の要素をHTMLから取得し、message という変数に入れる
+ 
+function ChangeColor(NewColor) {
+  message.style.color = NewColor
+}
+
+let count = 0
+const countDisplay = document.getElementById("count")
+
+increment.addEventListener("click", () => {
+  countDisplay.textContent = Number(countDisplay.textContent) + 1
+})
+
+reset.addEventListener("click", () => {
+  countDisplay.textContent = 0
+})
